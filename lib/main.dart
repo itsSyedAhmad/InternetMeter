62976fb2afd1e54d56705b27dbec9149b8a1d238
“itsSyedAhmad” <saifrock7185@gmail.com>
2025-04-19 14:34:53 +0530

    add android permission.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
-
-
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:flutter_internet_meter/foreground_task_service.dart';
 import 'package:flutter_internet_meter/storage_service.dart';
 import 'package:flutter_internet_meter/usage_data_screen.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  //permission
+  // Android 13+, you need to allow notification permission to display foreground service notification.
+  //
+  // iOS: If you need notification, ask for permission.
+  final NotificationPermission notificationPermission =
+      await FlutterForegroundTask.checkNotificationPermission();
+
+  if (notificationPermission != NotificationPermission.granted) {
+    await FlutterForegroundTask.requestNotificationPermission();
+  }
+
+  //init services
   await DataUsageStorageService.instance.init();
- 
   await SpeedMonitorService().startForegroundService();
-
-
 
   runApp(MaterialApp(home: UsageDataScreen()));
 }
